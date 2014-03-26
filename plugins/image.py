71e0739adb28621713f049775f5be0d8f5c7d2c3
Bill Mill <bill.mill@gmail.com>
2014-03-26 15:47:42 -0400

    add image plugin

---
+++ plugins/image.py
@@ -0,0 +1,30 @@
+"""!image <search term> return the first google image search result for <search term>"""
+
+from urllib import quote
+import re
+import requests
+from random import shuffle
+
+def image(searchterm, unsafe=False):
+    searchterm = quote(searchterm)
+
+    safe = "&safe=" if unsafe else "&safe=active"
+    searchurl = "https://www.google.com/search?tbm=isch&q={0}{1}".format(searchterm, safe)
+
+    # this is an old iphone user agent. Seems to make google return good results.
+    useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
+
+    result = requests.get(searchurl, headers={"User-agent": useragent}).text
+
+    images = re.findall(r'imgurl.*?(http.*?)\\', result)
+    shuffle(images)
+
+    return images[0] if images else ""
+
+def on_message(msg):
+    text = msg.get("text", "")
+    match = re.findall(r"!image (.*)", text)
+    if not match: return
+
+    searchterm = match[0]
+    return image(searchterm)
