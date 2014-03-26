d098855a15063c84749bcd97c5448498f357b685
Bill Mill <bill.mill@gmail.com>
2014-03-26 15:37:32 -0400

    use requests instead of urllib in gif

--- plugins/gif.py
+++ plugins/gif.py
@@ -1,8 +1,8 @@
 """!gif <search term> return the first google gif search result for <search term>"""
 
-import urllib2
 from urllib import quote
 import re
+import requests
 from random import shuffle
 
 def gif(searchterm, unsafe=False):
@@ -14,9 +14,7 @@ def gif(searchterm, unsafe=False):
     # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
 
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', useragent)]
-    result = opener.open(searchurl).read()
+    result = requests.get(searchurl, headers={"User-agent": useragent}).text
 
     gifs = re.findall(r'imgurl.*?(http.*?)\\', result)
     shuffle(gifs)
