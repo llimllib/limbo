9e638fa84dee4e3a9ede140a24e88a20fab38191
Bill Mill <bill.mill@gmail.com>
2014-03-27 08:55:03 -0400

    adding youtube plugin

---
+++ plugins/youtube.py
@@ -0,0 +1,28 @@
+import re
+from urllib import quote
+
+import requests
+
+def youtube(searchterm):
+    searchterm = quote(searchterm)
+    url = "https://gdata.youtube.com/feeds/api/videos?q={}&orderBy=relevance&alt=json"
+    url = url.format(searchterm)
+
+    j = requests.get(url).json()
+
+    results = j["feed"]
+    if "entry" not in results:
+        return "sorry, no videos found"
+
+    video = results["entry"][0]["link"][0]["href"]
+    video = re.sub("&feature=youtube_gdata", "", video)
+
+    return video
+
+def on_message(msg):
+    text = msg.get("text", "")
+    match = re.findall(r"!youtube (.*)", text)
+    if not match: return
+
+    searchterm = match[0]
+    return youtube(searchterm)
