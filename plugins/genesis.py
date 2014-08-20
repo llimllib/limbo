4888fb243a686a319756964e60dc57f382ec21dd
Bill Mill <bill.mill@gmail.com>
2014-08-20 09:44:42 -0400

    fix genesis plugin not to speak every time

--- plugins/genesis.py
+++ plugins/genesis.py
@@ -1,3 +1,5 @@
+"""!genesis returns a random sega genesis screenshot"""
+
 from bs4 import BeautifulSoup
 from urllib import quote
 import re
@@ -5,6 +7,7 @@ import requests
 from random import shuffle, randint
 
 def genesis():
+    # http://ascii.textfiles.com/archives/4365
     page = randint(1,8)
     r = requests.get("https://secure.flickr.com/photos/textfiles/sets/72157646180733361/page%s/"%page)
     soup = BeautifulSoup(r.text)
@@ -16,4 +19,8 @@ def genesis():
     return images[0] if images else ""
 
 def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!genesis", text)
+    if not match: return
+
     return genesis()
