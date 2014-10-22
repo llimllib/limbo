2e2f9ac5fec6b2e648ec59ea17e0703e0d22660c
Bill Mill <bill.mill@gmail.com>
2014-10-22 19:19:28 -0400

    add flip plugin

---
+++ plugins/flip.py
@@ -0,0 +1,16 @@
+"""!flip [alpha,beta,gamma] - flip a coin n times or shuffle a comma separated list"""
+import random
+import re
+
+def flip(lst):
+    random.shuffle(lst)
+    return ", ".join(lst)
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!flip( .*)?", text)
+    if not match: return
+
+    lst = ["heads", "tails"] if not match[0] else match[0].strip().split(',')
+
+    return flip(lst)
