26d265ec18011976cf8bd0410d08056a5dd597fd
Bill Mill <bill.mill@gmail.com>
2014-10-22 23:39:38 -0400

    better stock plugin

--- plugins/stock.py
+++ plugins/stock.py
@@ -10,15 +10,15 @@ def stockprice(ticker):
     soup = BeautifulSoup(requests.get(url.format(quote(ticker))).text)
 
     try:
-        ticker = re.findall("var _ticker = '(.*?)';", soup.text)[0]
+        company, ticker = re.findall(u"^(.+?)\xa0\xa0(.+?)\xa0", soup.text, re.M)[0]
         price = soup.select("#price-panel .pr span")[0].text
         change, pct = soup.select("#price-panel .nwp span")[0].text.split()
         pct.strip('()')
 
-        emoji = ":chart_with_upwards_trend:" if change.startswith("+") else ":chart_with_downwards_trend"
+        emoji = ":chart_with_upwards_trend:" if change.startswith("+") else ":chart_with_downwards_trend:"
 
-        return "{} {}: {} {} {} {}".format(emoji, ticker, price, change, pct, emoji)
-    except:
+        return "{} {} {}: {} {} {} {}".format(emoji, company, ticker, price, change, pct, emoji)
+    except Exception as e:
         return ""
 
 def on_message(msg, server):
