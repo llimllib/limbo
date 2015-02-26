73c5be569c7ab647eea101ee3430f8197628653f
Bill Mill <bill.mill@gmail.com>
2015-02-26 13:13:29 -0500

    leave exception handling to limbo

--- limbo/plugins/stock.py
+++ limbo/plugins/stock.py
@@ -4,24 +4,22 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from urllib import quote
+from logging import logging
 
+logger = logging.getLogger(__name__)
 
 def stockprice(ticker):
     url = "https://www.google.com/finance?q={0}"
-    print(url.format(quote(ticker)))
     soup = BeautifulSoup(requests.get(url.format(quote(ticker))).text)
 
-    try:
-        company, ticker = re.findall(u"^(.+?)\xa0\xa0(.+?)\xa0", soup.text, re.M)[0]
-        price = soup.select("#price-panel .pr span")[0].text
-        change, pct = soup.select("#price-panel .nwp span")[0].text.split()
-        pct.strip('()')
+    company, ticker = re.findall(u"^(.+?)\xa0\xa0(.+?)\xa0", soup.text, re.M)[0]
+    price = soup.select("#price-panel .pr span")[0].text
+    change, pct = soup.select("#price-panel .nwp span")[0].text.split()
+    pct.strip('()')
 
-        emoji = ":chart_with_upwards_trend:" if change.startswith("+") else ":chart_with_downwards_trend:"
+    emoji = ":chart_with_upwards_trend:" if change.startswith("+") else ":chart_with_downwards_trend:"
 
-        return "{0} {1} {2}: {3} {4} {5} {6}".format(emoji, company, ticker, price, change, pct, emoji)
-    except Exception:
-        return ""
+    return "{0} {1} {2}: {3} {4} {5} {6}".format(emoji, company, ticker, price, change, pct, emoji)
 
 
 def on_message(msg, server):
