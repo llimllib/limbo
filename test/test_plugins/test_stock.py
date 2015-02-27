bb1cd85d8163a30f2b9f283b253726dba5ad3c8f
Bill Mill <bill.mill@gmail.com>
2015-02-26 21:18:06 -0500

    some more stock tests

--- test/test_plugins/test_stock.py
+++ test/test_plugins/test_stock.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import os
 import sys
 
@@ -6,14 +7,28 @@ import vcr
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
-FIX = os.path.join(DIR, "../../test/fixtures")
 
 from stock import on_message, stockprice
 
 def test_apple():
-    with vcr.use_cassette('test/fixtures/apple.yaml'):
+    with vcr.use_cassette('test/fixtures/stock_apple.yaml'):
         ret = on_message({"text": u"$aapl"}, None)
         assert ':chart_with_upwards_trend:' in ret
         assert 'Apple Inc.' in ret
         assert '130.41' in ret
         assert '+1.62' in ret
+
+def test_nonexistent():
+    with vcr.use_cassette('test/fixtures/stock_none'):
+        ret = on_message({"text": u"bana"}, None)
+        eq_(ret, None)
+
+def test_unicode():
+    with vcr.use_cassette('test/fixtures/stock_unicode.yaml'):
+        ret = on_message({"text": u"$äapl"}, None)
+        eq_(ret, '')
+
+def test_multiple():
+    with vcr.use_cassette('test/fixtures/stock_multiple.yaml'):
+        ret = on_message({"text": u"$goog $aapl"}, None)
+        assert 'Google Inc' in ret
