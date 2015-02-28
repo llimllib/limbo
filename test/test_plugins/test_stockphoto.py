8304b84b1ead88ce792692cfc939fe57a3de0308
Bill Mill <bill.mill@gmail.com>
2015-02-27 21:29:19 -0500

    add stockphoto tests

---
+++ test/test_plugins/test_stockphoto.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+
+from nose.tools import eq_
+import vcr
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
+
+from map import on_message
+
+def test_unicode():
+    ret = on_message({"text": u"!map Moscow, россия"}, None)
+    assert "googleapis" in ret
+    assert "Moscow" in ret
