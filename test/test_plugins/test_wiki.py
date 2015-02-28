d85d3d7810b22345d9735a17afc768458752f237
Bill Mill <bill.mill@gmail.com>
2015-02-27 21:43:45 -0500

    add wiki tests

---
+++ test/test_plugins/test_wiki.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+
+from nose.tools import eq_
+import vcr
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
+
+from wiki import on_message
+
+def test_basic():
+    with vcr.use_cassette('test/fixtures/wiki_basic.yaml'):
+        ret = on_message({"text": u"!wiki dog"}, None)
+        assert "member of the canidae family" in ret
+        assert "http://en.wikipedia.org/wiki/Dog" in ret
+
+def test_unicode():
+    with vcr.use_cassette('test/fixtures/wiki_unicode.yaml'):
+        ret = on_message({"text": u"!wiki नेपाल"}, None)
+        # not blowing up == success
