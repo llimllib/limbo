e9d671e2aaac25df63562aeedd8983483e56ad81
Bill Mill <bill.mill@gmail.com>
2015-02-27 21:50:05 -0500

    add youtube tests

---
+++ test/test_plugins/test_youtube.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+
+from nose.tools import eq_
+import vcr
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
+
+from youtube import on_message
+
+def test_basic():
+    with vcr.use_cassette('test/fixtures/youtube_basic.yaml'):
+        ret = on_message({"text": u"!youtube live long and prosper"}, None)
+        assert ret == "https://www.youtube.com/watch?v=DyiWkWcR86I"
+
+def test_unicode():
+    with vcr.use_cassette('test/fixtures/youtube_unicode.yaml'):
+        ret = on_message({"text": u"!wiki 崖の上のポニョ"}, None)
+        # not blowing up == success
