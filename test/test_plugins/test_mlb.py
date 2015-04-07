1f1b2d2e8d1b8da98c6758c7484325309ddb637b
g2graman <fran.gramano@mail.utoronto.ca>
2015-04-07 16:05:34 -0400

    Fixes type in test (oops)

--- test/test_plugins/test_mlb.py
+++ test/test_plugins/test_mlb.py
@@ -13,4 +13,4 @@ from mlb import on_message
 def test_basic():
     with vcr.use_cassette('test/fixtures/mlb_basic.yaml'):
         ret = on_message({"text": u"!mlb Red Sox"}, None)
-        assert "Boston Red Sex" in ret
+        assert "Boston Red Sox" in ret
