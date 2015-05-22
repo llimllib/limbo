18c518f3edc2e2d67ac99081963561f08d833983
Bill Mill <bill@billmill.org>
2015-05-21 21:43:40 -0400

    add SearchList to test

---
+++ test/SearchList.py
@@ -0,0 +1,16 @@
+# copied this stupid-ass interface from SlackClient. Just use a damned dictionary!
+class SearchList(list):
+    def find(self, name):
+        items = []
+        for child in self:
+            if child.__class__ == self.__class__:
+                items += child.find(name)
+            else:
+                if child == name:
+                    items.append(child)
+
+        if len(items) == 1:
+            return items[0]
+        elif items != []:
+            return items
+
