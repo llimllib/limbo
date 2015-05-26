9c68980ea1983e3bbe7d5475c17f253e7f953b7b
Bill Mill <bill@billmill.org>
2015-05-25 22:02:51 -0400

    note 3.4 support in setup.py

--- setup.py
+++ setup.py
@@ -40,5 +40,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ),
 )
