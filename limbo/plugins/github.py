05169da928eb3aa3e4e34ab7e3810447cf226659
Bill Mill <bill@billmill.org>
2015-09-23 15:07:04 -0400

    add issue search

--- limbo/plugins/github.py
+++ limbo/plugins/github.py
@@ -14,10 +14,12 @@ class Github(object):
     def __init__(self, username, password):
         self.auth = username, password
 
-    def _get(self, url_fragment):
+    def _get(self, url_fragment, **params):
+        return requests.get( HUB_URL.format(url_fragment), auth=self.auth, params=params).json()
         return requests.get(
                  HUB_URL.format(url_fragment),
-                 auth=self.auth
+                 auth=self.auth,
+                 params=params
                ).json()
 
     def _post(self, url_fragment, data={}, **params):
@@ -39,7 +41,23 @@ class Github(object):
                     "title": title,
                     "body": body}))
 
+    def search_issue_in_repo(self, repo, query):
+        return self._get(
+                'search/issues',
+                q="{} repo:{}".format(query, repo))
+
 HUB = Github(os.environ.get("GITHUB_USER"), os.environ.get("GITHUB_PASS"))
+
+def format_issue(issue_json):
+    return {
+        "author_icon": issue_json["user"]["avatar_url"],
+        "author_name": issue_json["user"]["login"],
+        "author_link": issue_json["user"]["html_url"],
+        "fallback": issue_json["title"],
+        "title": "[{}] {}".format(issue_json["number"], issue_json["title"]),
+        "title_link": issue_json["html_url"],
+        "color": "good"
+    }
 
 def github(cmd, *args):
     if cmd == "issues":
@@ -51,16 +69,7 @@ def github(cmd, *args):
         else:
             text = "{} open issues".format(l)
 
-        attachments = json.dumps([{
-                "author_icon": i["user"]["avatar_url"],
-                "author_name": i["user"]["login"],
-                "author_link": i["user"]["html_url"],
-                "fallback": i["title"],
-                "title": "[{}] {}".format(i["number"], i["title"]),
-                "title_link": i["html_url"],
-                "color": "good"
-            }
-            for i in issues[:5]])
+        attachments = json.dumps([format_issue(i) for i in issues[:5]])
 
         return {
             "attachments": attachments,
@@ -70,21 +79,29 @@ def github(cmd, *args):
         repo = args[0]
         title = ' '.join(args[1:])
         issue = HUB.create_issue(repo, title)
-        attachments = json.dumps([{
-                "author_icon": issue["user"]["avatar_url"],
-                "author_name": issue["user"]["login"],
-                "author_link": issue["user"]["html_url"],
-                "fallback": issue["title"],
-                "title": "[{}] {}".format(issue["number"], issue["title"]),
-                "title_link": issue["html_url"],
-                "color": "good"
-            }])
+        attachments = json.dumps([format_issue(issue)])
 
         return {
             "attachments": attachments,
             "text": "",
         }
+    if cmd in ["search"]:
+        repo = args[0]
+        query = ' '.join(args[1:])
+        response = HUB.search_issue_in_repo(repo, query)
 
+        if response["total_count"] == 0:
+            return {
+                "text": "sorry, no issues found"
+            }
+
+        issues = response["items"]
+        attachments = json.dumps([format_issue(i) for i in issues[:5]])
+        text = "Found {} items".format(response["total_count"])
+        return {
+            "attachments": attachments,
+            "text": text
+        }
 
 def on_message(msg, server):
     text = msg.get("text", "")
