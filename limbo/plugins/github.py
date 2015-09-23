19904fe51d3199480838995110052a06e089b927
Bill Mill <bill@billmill.org>
2015-09-23 13:59:41 -0400

    add issue creation

--- limbo/plugins/github.py
+++ limbo/plugins/github.py
@@ -20,9 +20,24 @@ class Github(object):
                  auth=self.auth
                ).json()
 
+    def _post(self, url_fragment, data={}, **params):
+        return requests.post(
+                 HUB_URL.format(url_fragment),
+                 auth=self.auth,
+                 data=data,
+                 params=params
+               ).json()
+
     def issues(self, repo):
         # defaults to only open issues
         return self._get('repos/{}/issues'.format(repo))
+
+    def create_issue(self, repo, title, body=''):
+        return self._post(
+                'repos/{}/issues'.format(repo),
+                data=json.dumps({
+                    "title": title,
+                    "body": body}))
 
 HUB = Github(os.environ.get("GITHUB_USER"), os.environ.get("GITHUB_PASS"))
 
@@ -40,9 +55,9 @@ def github(cmd, *args):
                 "author_icon": i["user"]["avatar_url"],
                 "author_name": i["user"]["login"],
                 "author_link": i["user"]["html_url"],
-                "fallback": i["body"],
+                "fallback": i["title"],
                 "title": "[{}] {}".format(i["number"], i["title"]),
-                "title_link": i["url"],
+                "title_link": i["html_url"],
                 "color": "good"
             }
             for i in issues[:5]])
@@ -51,6 +66,25 @@ def github(cmd, *args):
             "attachments": attachments,
             "text": text,
         }
+    if cmd in ["create", "new"]:
+        repo = args[0]
+        title = ' '.join(args[1:])
+        issue = HUB.create_issue(repo, title)
+        attachments = json.dumps([{
+                "author_icon": issue["user"]["avatar_url"],
+                "author_name": issue["user"]["login"],
+                "author_link": issue["user"]["html_url"],
+                "fallback": issue["title"],
+                "title": "[{}] {}".format(issue["number"], issue["title"]),
+                "title_link": issue["html_url"],
+                "color": "good"
+            }])
+
+        return {
+            "attachments": attachments,
+            "text": "",
+        }
+
 
 def on_message(msg, server):
     text = msg.get("text", "")
