bf4858fff6ece7d98aea1a7c71a859c0f33536c6
TetraEtc <administrator@tetraetc.com>
2015-10-30 19:12:43 +1000

    Minor Updates
    Redundant & symbol in URL.
    Added formatting to the error message.

--- limbo/plugins/weather.py
+++ limbo/plugins/weather.py
@@ -27,12 +27,12 @@ def weather(searchterm):
     weather_api_key = os.environ.get("WEATHER_API_KEY")
     if not weather_api_key:
         return "Please set as the WEATHER_API_KEY environment variable to a " \
-               "valid (free) OpenWeatherMap API key: " \
-               "http://openweathermap.org/appid#get"
+               "valid (free) OpenWeatherMap API key " \
+               "<http://openweathermap.org/appid#get|available here>"
 
     searchterm = quote(searchterm)
     url = 'http://api.openweathermap.org/data/2.5/forecast/daily?'\
-          'q={0}&cnt=5&mode=json&units=imperial&&APPID={1}'.format(
+          'q={0}&cnt=5&mode=json&units=imperial&APPID={1}'.format(
                   searchterm, weather_api_key)
 
     dat = requests.get(url).json()
