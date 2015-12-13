dbb3c85b529ca3917362badd90868e593328946e
TetraEtc <administrator@tetraetc.com>
2015-12-13 12:23:36 -0500

    Add an urban dictionary plugin

---
+++ limbo/plugins/urban.py
@@ -0,0 +1,29 @@
+"""!urban <term> returns the urban dictionary definition and example of a term"""
+import requests
+import re
+import sys
+
+def quote(string):
+    lines = string.split("\n")
+    return "\n".join("> _{0}_".format(l.strip()) for l in lines)
+
+def urban(term):
+    baseurl = u"http://api.urbandictionary.com/v0/define?term={0}"
+    data = requests.get(baseurl.format(term)).json()
+
+    try:
+        result = data['list'][0]
+        result["example"] = quote(result.get("example", ""))
+        definition = (u"*{word}*: {definition}.\n"
+                        "*Example:*\n{example}".format(**result))
+        return definition
+    except IndexError:
+        return ":cry: No results found for {0}, please try again".format(term)
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!urban (.*)", text)
+    if not match:
+        return
+    searchterm = match[0]
+    return urban(searchterm)
---
+++ test/test_plugins/test_urban.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+
+from nose.tools import eq_
+import vcr
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
+
+from urban import on_message
+
+def test_unicode():
+    ret = on_message({"text": u"!urban Mötörhead"}, None)
+    # not blowing up == success, for our purposes
+
+def test_message():
+    ret = on_message({"text": u"!urban Test"}, None)
+    assert "Example" in ret
