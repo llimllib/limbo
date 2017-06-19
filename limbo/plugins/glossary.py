e1178e217597e020a2aa4d59b7456318d606243d
Ryan Nagle <rmnagle@gmail.com>
2017-06-19 09:19:23 -0500

    fix file handling, make the 'remove' command work

--- limbo/plugins/glossary.py
+++ limbo/plugins/glossary.py
@@ -11,30 +11,34 @@ import re
 GLOSSARY_FILE = os.environ.get('LIMBO_GLOSSARY_FILE', "/tmp/glossary.json")
 
 
-def define(term, definition):
-    with open(GLOSSARY_FILE, 'w+') as f:
+def add(term, definition):
+    with open(GLOSSARY_FILE, 'r') as f:
         try:
             glossary = json.loads(f.read())
         except ValueError:
             glossary = {}
 
-        action = bool(glossary.get(term, False)) and "Updated" or "Added"
-        glossary[term] = definition
-        f.write(json.dumps(glossary))
-        return "%s a definition for %s"% (action, term)
+    action = bool(glossary.get(term, False)) and "Updated" or "Added"
+    glossary[term] = definition
 
+    with open(GLOSSARY_FILE, 'w') as f:
+        f.write(json.dumps(glossary))
+
+    return "%s a definition for %s"% (action, term)
 
 def remove(term):
-    with open(GLOSSARY_FILE, 'w+') as f:
+    with open(GLOSSARY_FILE, 'r') as f:
         try:
             glossary = json.loads(f.read())
         except ValueError:
             return "There aren't any definitions to remove yet! Add a definition with '!glossary add <term>: <definition>'"
 
-        del(glossary[term])
-        f.write(json.dumps(glossary))
-        return "Removed definition for %s" % term
+    del(glossary[term])
 
+    with open(GLOSSARY_FILE, 'w') as f:
+        f.write(json.dumps(glossary))
+
+    return "Removed definition for %s" % term
 
 def lookup(term):
     not_found = "I don't know that term. Add a definition with '!glossary add <term>: <definition>'."
@@ -55,17 +59,24 @@ def lookup(term):
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    match = re.match(r'!glossary (add\s+)?([^:]*)(.*)?', text)
+    match = re.match(r'!gloss(ary)? (add|remove\s+)?([^:]*)(.*)?', text, re.IGNORECASE)
 
     if not match:
         return
 
+    if not os.path.isfile(GLOSSARY_FILE):
+        open(GLOSSARY_FILE, 'a').close()
+
     groups = match.groups()
-    if groups[0]:
-        term = groups[1]
-        definition = groups[2].lstrip(':').strip()
-        return define(term, definition)
+    if groups[1]:
+        action = groups[1].strip()
+        if action == 'add':
+            term = groups[2].strip()
+            definition = groups[3].lstrip(':').strip()
+            return add(term, definition)
+        elif action == 'remove':
+            return remove(groups[2])
     else:
-        return lookup(groups[1])
+        return lookup(groups[2])
 
 on_bot_message = on_message
