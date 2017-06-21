60cc30026db7c971efe6c162be3b1335311d65db
Bill Mill <bill@billmill.org>
2017-06-20 21:33:02 -0400

    bump to 5.6.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except:
 
 setup(
     name='limbo',
-    version='5.6.3',
+    version='5.6.4',
     description='Simple and Clean Slack Chatbot',
     long_description=longdesc,
     author='Bill Mill',
