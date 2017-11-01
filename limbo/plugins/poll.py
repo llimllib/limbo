a7faa7178ebca094119e63638ad529cf5855b2b9
Bill Mill <bill@billmill.org>
2017-10-31 23:11:58 -0400

    refactor poll a bit

--- limbo/plugins/poll.py
+++ limbo/plugins/poll.py
@@ -1,26 +1,41 @@
 """!poll 'question' 'option A' 'option b' ... 'option x' create a poll"""
 
-import re
-import requests
+import argparse
 import json
+import re
+import shlex
 
-numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "keycap_ten"]
+POLL_EMOJIS = ["one", "two", "three", "four", "five", "six", "seven", "eight",
+               "nine", "keycap_ten"]
 
+ARGPARSE = argparse.ArgumentParser()
+ARGPARSE.add_argument('poll', nargs='*')
 
-def poll(question, answers, msg, server):
-    result = "Poll: {}\n".format(question)
-    for i in range(len(answers)):
-        result += ":{}: {}\n".format(numbers[i], answers[i])
-    
+def remove_smart_quotes (text):
+    return text.replace(u"\u2018", "'") \
+        .replace(u"\u2019", "'") \
+        .replace(u"\u201c", '"') \
+        .replace(u"\u201d", '"')
+
+def poll(poll, msg, server):
+    """Given a question and answers, present a poll"""
+    poll = remove_smart_quotes(poll.replace(u"\u2014", u"--"))
+
+    args = ARGPARSE.parse_args(shlex.split(poll)).poll
+
+    result = ["Poll: {}\n".format(args[0])]
+    for emoji, answer in zip(POLL_EMOJIS, args[1:]):
+        result.append(":{}: {}\n".format(emoji, answer))
+
     # for this we are going to need to post the message to Slack via Web API in order to
     #  get the TS and add reactions
     msg_posted = server.slack.post_message(
-            msg['channel'],
-            result,
-            as_user=server.slack.username)
+        msg['channel'],
+        "".join(result),
+        as_user=server.slack.username)
     ts = json.loads(msg_posted)["ts"]
-    for i in range(len(answers)):
-        server.slack.post_reaction(msg['channel'], ts, numbers[i])
+    for i in range(len(args) - 1):
+        server.slack.post_reaction(msg['channel'], ts, POLL_EMOJIS[i])
 
 
 def on_message(msg, server):
@@ -29,12 +44,7 @@ def on_message(msg, server):
     if not match:
         return
 
-    text = match[0]
-    blocks = text.split()
-    blocks = map(lambda x: x[1:-1], blocks)
-    if len(blocks) < 3:
-        return  # needs at least a question and 2 options to choose from
-    return poll(blocks[0], blocks[1:], msg, server)
+    return poll(match[0], msg, server)
 
 
 on_bot_message = on_message
