30ecb1479e1cda414ff9156ccfd2936d529b7b81
Eduardo Soares <eduardosoares.pt@gmail.com>
2017-11-04 01:05:27 +0000

    Add upper bound number of args validation in poll plugin

--- limbo/plugins/poll.py
+++ limbo/plugins/poll.py
@@ -8,7 +8,7 @@ import shlex
 POLL_EMOJIS = ["one", "two", "three", "four", "five", "six", "seven", "eight",
                "nine", "keycap_ten"]
 
-ERROR_WRONG_NUMBER_OF_ARGUMENTS = "A Poll must have at least a question and two options"
+ERROR_WRONG_NUMBER_OF_ARGUMENTS = "A Poll must have at least a question and between two to ten options"
 
 ARGPARSE = argparse.ArgumentParser()
 ARGPARSE.add_argument('poll', nargs='*')
@@ -25,7 +25,7 @@ def poll(poll, msg, server):
 
     args = ARGPARSE.parse_args(shlex.split(poll)).poll
 
-    if len(args) < 3:
+    if not (3 <= len(args) <= len(POLL_EMOJIS) + 1):
         return ERROR_WRONG_NUMBER_OF_ARGUMENTS
 
     result = ["Poll: {}\n".format(args[0])]
