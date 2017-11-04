bb1dcee8df07744c653b2f21c28bc73e508569d4
Bill Mill <bill@billmill.org>
2017-11-04 14:55:58 -0400

    and a bit of formatting on test

--- test/test_plugins/poll_test.py
+++ test/test_plugins/poll_test.py
@@ -9,17 +9,19 @@ from poll import on_message, ERROR_WRONG_NUMBER_OF_ARGUMENTS
 
 
 def test_poll():
-    # wrong number of args
     ret = on_message({"text": u"!poll ''"}, None)
     assert ERROR_WRONG_NUMBER_OF_ARGUMENTS in ret
-    # one more than it can be
-    ret = on_message({"text": u"!poll 'q?' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11'"}, None)
+
+    ret = on_message({
+        "text": u"!poll 'q?' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11'"
+        }, None)
     assert ERROR_WRONG_NUMBER_OF_ARGUMENTS in ret
 
     fakeserver = FakeServer()
-    ret = on_message({"text": u"!poll 'Hello?' 'is it me your looking for' 'i can see it in your eyes'",
-                      "channel": "xyz"}, fakeserver)
-    assert None == ret
-    assert len(fakeserver.slack.posted_reactions) is 1  # one timestamp
-    assert len(list(fakeserver.slack.posted_reactions.values())[0]) is 2 # two reactions
-    assert len(fakeserver.slack.posted_messages) is 1  # the question and the two answers is a single message
+    ret = on_message({
+        "text": u"!poll 'Hello?' 'is it me your looking for' 'i can see it",
+        "channel": "xyz"}, fakeserver)
+    assert ret is None
+    assert len(fakeserver.slack.posted_reactions) == 1
+    assert len(list(fakeserver.slack.posted_reactions.values())[0]) == 2
+    assert len(fakeserver.slack.posted_messages) == 1
