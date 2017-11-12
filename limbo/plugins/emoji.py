e5e3d6558c76bf2f44b6071a040a6d4bac38b9bc
Bill Mill <bill@billmill.org>
2017-11-12 09:59:24 -0500

    include custom emoji in "!emoji"

--- limbo/plugins/emoji.py
+++ limbo/plugins/emoji.py
@@ -1,16 +1,36 @@
 """!emoji <n> will return n random emoji"""
+import json
 import re
 import random
 from emojicodedict import emojiCodeDict
 
+CUSTOM_EMOJI = None
+
 def randomelt(dic):
+    """return a random element from a dictionary with replacement"""
     keys = list(dic.keys())
     i = random.randint(0, len(keys) - 1)
     return dic[keys[i]]
 
-def emoji(n=1):
+def get_custom_emoji(server):
+    """get a workspace's custom emoji and store them in CUSTOM_EMOJI"""
+    emoji_res = json.loads(server.slack.api_call("emoji.list"))
+    CUSTOM_EMOJI = []
+    for emo in emoji_res["emoji"]:
+        url = emoji_res["emoji"][emo]
+        emoji = ":{}:".format(emo)
+        # duplicate emoji will start with "alias:" instead of an "http"; we
+        # don't want to include dupes in our list so we don't bias towards them
+        # https://api.slack.com/methods/emoji.list
+        if url.startswith("http") and emoji not in emojiCodeDict:
+            CUSTOM_EMOJI.append(emoji)
+
+def emoji_list(server, n=1):
+    """return a list of `n` random emoji"""
+    if CUSTOM_EMOJI is None:
+        get_custom_emoji(server)
     emoji = []
-    for i in range(n):
+    for _ in range(n):
         emoji.append(randomelt(emojiCodeDict))
 
     return "".join(emoji)
@@ -23,6 +43,6 @@ def on_message(msg, server):
 
     n = 1 if not match[0][1] else int(match[0][1])
 
-    return emoji(n)
+    return emoji_list(server, n)
 
 on_bot_message = on_message
