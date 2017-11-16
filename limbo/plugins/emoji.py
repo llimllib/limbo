a50078617cfaad8f1f3c80b06b4124433fb7bde9
Bill Mill <bill@billmill.org>
2017-11-15 21:47:04 -0500

    include custom emoji in "!emoji"
    bump version to 6.4.2
    
    closes #145

--- limbo/plugins/emoji.py
+++ limbo/plugins/emoji.py
@@ -1,19 +1,52 @@
 """!emoji <n> will return n random emoji"""
+import json
 import re
 import random
+from time import time
 from emojicodedict import emojiCodeDict
 
-def randomelt(dic):
-    keys = list(dic.keys())
-    i = random.randint(0, len(keys) - 1)
-    return dic[keys[i]]
+EMOJI = None
 
-def emoji(n=1):
-    emoji = []
-    for i in range(n):
-        emoji.append(randomelt(emojiCodeDict))
+class EmojiCache(object):
+    def __init__(self, server):
+        self.server = server
+        self.last_updated = 0
+        self.emoji = []
 
-    return "".join(emoji)
+    def __len__(self):
+        return len(self.emoji)
+
+    def __getitem__(self, idx):
+        return self.emoji[idx]
+
+    def __setitem__(self, idx, val):
+        self.emoji[idx] = val
+
+    def get(self, n):
+        if time() - self.last_updated > 60 * 60:
+            self.update()
+            self.last_updated = time()
+        random.shuffle(self.emoji)
+        return "".join(self.emoji[:n])
+
+    def update(self):
+        self.emoji = list(emojiCodeDict.keys())
+        emoji_res = json.loads(self.server.slack.api_call("emoji.list"))
+        for emo in emoji_res["emoji"]:
+            url = emoji_res["emoji"][emo]
+            emoji = ":{}:".format(emo)
+            # duplicate emoji will start with "alias:" instead of an "http"; we
+            # don't want to include dupes in our list so we don't bias towards them
+            # https://api.slack.com/methods/emoji.list
+            if url.startswith("http") and emoji not in emojiCodeDict:
+                self.emoji.append(emoji)
+
+def emoji_list(server, n=1):
+    """return a list of `n` random emoji"""
+    global EMOJI
+    if EMOJI is None:
+        EMOJI = EmojiCache(server)
+    return EMOJI.get(n)
 
 def on_message(msg, server):
     text = msg.get("text", "")
@@ -23,6 +56,6 @@ def on_message(msg, server):
 
     n = 1 if not match[0][1] else int(match[0][1])
 
-    return emoji(n)
+    return emoji_list(server, n)
 
 on_bot_message = on_message
