c01d04067ad69c12633fb0fad0a07de115264ba4
Bill Mill <bill@billmill.org>
2017-11-16 22:31:22 -0500

    update tests

--- test/test_plugins/test_weather.py
+++ test/test_plugins/test_weather.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
+import json
 import os
 import sys
 
+import limbo
 import vcr
 
 DIR = os.path.dirname(os.path.realpath(__file__))
@@ -9,12 +11,22 @@ sys.path.insert(0, os.path.join(DIR, '../../limbo/plugins'))
 
 from weather import on_message
 
+def msgobj(msg):
+    return {
+        "text": msg,
+        "channel": "abc123"
+    }
+
 def test_basic():
+    server = limbo.FakeServer()
     with vcr.use_cassette('test/fixtures/weather_basic.yaml'):
-        ret = on_message({"text": u"!weather Oahu, HI"}, None)
-        assert ":cloud: Sat 71" in ret
+        on_message(msgobj(u"!weather Oahu, HI"), server)
+        attachment = json.loads(server.slack.posted_messages[0][1]['attachments'])[0]
+        assert "Weather for Honolulu, HI" in attachment['pretext']
+        assert attachment['fields'][0]['value'] == ':sun_small_cloud: 73°f'
 
 def test_unicode():
+    server = limbo.FakeServer()
     with vcr.use_cassette('test/fixtures/weather_unicode.yaml'):
-        ret = on_message({"text": u"!weather Provençal"}, None)
+        on_message(msgobj(u"!weather กรุงเทพมหานคร"), server)
         # not blowing up == success
