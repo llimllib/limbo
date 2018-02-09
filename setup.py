473f503b6c8f078f048870bcae683432b7a11cfc
Bill Mill <bill@billmill.org>
2018-02-09 15:42:44 -0500

    better weather icons
    * better weather icons
    * fix one broken emoji string
    * bump version to 6.5.1
    
    closes #155

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except:
 
 setup(
     name='limbo',
-    version='6.5.0',
+    version='6.5.1',
     description='Simple and Clean Slack Chatbot',
     long_description=longdesc,
     author='Bill Mill',
