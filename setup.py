84237dfd1e3362984e40f259add9bac424258f7e
Hugo <hugovk@users.noreply.github.com>
2018-10-29 22:21:33 +0200

    Update Trove classifiers and add python_requires

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     include_package_data=True,
     install_requires=required,
     license='MIT',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -45,6 +46,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     keywords="slack chatbot chat limbo",
