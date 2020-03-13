8175dbfaa6de901280c676d94871618f5eae0556
Bill Mill <bill@billmill.org>
2020-03-13 14:54:59 -0400

    bump version to 7.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except:
 
 setup(
     name="limbo",
-    version="7.1.0",
+    version="7.2.0",
     description="Simple and Clean Slack Chatbot",
     long_description=longdesc,
     author="Bill Mill",
