ce72754decb64914a1b1b5cdbf1f6d57dfcad8a9
Bill Mill <bill@billmill.org>
2020-05-04 22:12:15 -0400

    remove python 2 support

--- setup.py
+++ setup.py
@@ -9,12 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-PYTHON3 = sys.version_info[0] > 2
-
 required = []
-if not PYTHON3:
-    required += ["importlib>=1.0.4"]
-
 packages = ["limbo", "limbo.plugins"]
 
 try:
@@ -36,18 +31,18 @@ setup(
     include_package_data=True,
     install_requires=required,
     license="MIT",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=3.4",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     keywords="slack chatbot chat limbo",
--- test/test_cmd.py
+++ test/test_cmd.py
@@ -9,31 +9,38 @@ TESTPLUGINS = os.path.join(DIR, "plugins")
 
 # http://stackoverflow.com/a/13160748/42559
 def sh(cmd):
-    # slex.split can only handle unicode strings in python3, and only byte
-    # strings in python2
-    if sys.version_info[0] > 2:
-        cmd = cmd.decode("utf8")
+    # shlex.split can only handle unicode strings in python3
+    cmd = cmd.decode("utf8")
 
     proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
     output = proc.communicate()[0].decode("utf8")
     ret = proc.returncode
     return output, ret
 
+
 def test_cmd():
     msg = u"!echo Iñtërnâtiônàlizætiøn"
-    out, ret = sh(u"bin/limbo -c '{0}' --pluginpath {1}".format(msg, TESTPLUGINS).encode("utf8"))
+    out, ret = sh(
+        u"bin/limbo -c '{0}' --pluginpath {1}".format(msg, TESTPLUGINS).encode("utf8")
+    )
     out = out.strip()
     assert out == msg
     assert ret == 0
 
+
 def test_repl():
     msg = u"!echo Iñtërnâtiônàlizætiøn"
-    proc = subprocess.Popen(["bin/limbo", "-t", "--pluginpath", TESTPLUGINS], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    proc = subprocess.Popen(
+        ["bin/limbo", "-t", "--pluginpath", TESTPLUGINS],
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+    )
     out = proc.communicate(msg.encode("utf8"))[0]
     out = out.strip().decode("utf8")
     assert out == u"limbo> {0}\nlimbo>".format(msg)
     ret = proc.returncode
     assert ret == 0
+
 
 #  XXX: TODO
 # def test_hook():
